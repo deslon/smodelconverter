6823d73229a58cbbcf584d1ee4ff751a5290ec59
eduardodoria <doriaeduardo@gmail.com>
2018-07-22 23:04:33 -0300

    Updated SMODEL file generation

--- main.cpp
+++ main.cpp
@@ -23,27 +23,44 @@ struct Vector2 {
 
 struct MeshVertex {
     Vector3 vertex;
-    Vector2 texCoords;
+    Vector2 texcoord;
     Vector3 normal;
     Vector3 tangent;
     Vector3 bitangent;
 };
 
-struct MeshNode {
-    std::vector<MeshVertex> meshVertices;
+struct MeshMaterial {
+    int type;
+    std::string texture;
 };
 
+struct MeshNode {
+    std::vector<MeshVertex> meshVertices;
+    std::vector<unsigned int> indices;
+    std::vector<MeshMaterial> materials;
+};
 
-std::string directory;
-std::vector<MeshNode> meshNodes;
+struct MeshData {
+    std::vector<MeshNode> meshNodes;
+};
 
 void writeMeshVerticesVector(std::ostream& os, const std::vector<MeshVertex> &vec);
+void writeIndicesVector(std::ostream& os, const std::vector<unsigned int> &vec);
+void writeString(std::ostream& os, const std::string &str);
+void writeMeshMaterialsVector(std::ostream& os, const std::vector<MeshMaterial> &vec);
 void writeMeshNodesVector(std::ostream& os, const std::vector<MeshNode> &vec);
+
 void readMeshVerticesVector(std::istream& is, std::vector<MeshVertex> &vec);
+void readIndicesVector(std::istream& is, std::vector<unsigned int> &vec);
+void readString(std::istream& is, std::string &str);
+void readMeshMaterialsVector(std::istream& is, std::vector<MeshMaterial> &vec);
 void readMeshNodesVector(std::istream& is, std::vector<MeshNode> &vec);
 
 void processNode(aiNode *node, const aiScene *scene);
 MeshNode processMesh(aiMesh *mesh, const aiScene *scene);
+std::vector<MeshMaterial> loadMaterialTextures(aiMaterial *mat, aiTextureType assimp_type, int type);
+
+MeshData meshdata;
 
 int main (int argc, char *argv[]){
     if (argc < 2){
@@ -54,49 +71,57 @@ int main (int argc, char *argv[]){
     std::string path = argv[1];
 
     Assimp::Importer importer;
-    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_CalcTangentSpace | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices);
 
     if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
         printf("ERROR::ASSIMP:: %s\n", importer.GetErrorString());
         return 1;
     }
 
-    directory = path.substr(0, path.find_last_of('/'));
+    //directory = path.substr(0, path.find_last_of('/'));
 
     processNode(scene->mRootNode, scene);
 
-    int version = 5;
+    int version = 1;
 
     std::ofstream os;
     os.open("test.smodel", std::ios::out | std::ios::binary);
     os.write("SMODEL", sizeof(char) * 6);
     os.write((char*)&version, sizeof(int));
-    writeMeshNodesVector(os, meshNodes);
+    writeMeshNodesVector(os, meshdata.meshNodes);
     os.close();
 
     char* readsig= new char[6];
     int readversion;
-    std::vector<MeshNode> meshNodesTest;
+    MeshData meshDataTest;
 
     std::ifstream is;
     is.open("test.smodel", std::ios::in | std::ios::binary);
     is.read(readsig, sizeof(char) * 6);
     is.read((char*)&readversion, sizeof(int));
-    readMeshNodesVector(is, meshNodesTest);
+    readMeshNodesVector(is, meshDataTest.meshNodes);
     is.close();
 
 
-    for (int i = 0; i < meshNodes.size(); i++){
-        printf("Vertex count: %i\n", (int)meshNodes[i].meshVertices.size());
-        printf("Vertex: x: %f, y: %f, z: %f\n", meshNodes[i].meshVertices[0].vertex.x, meshNodes[i].meshVertices[0].vertex.y, meshNodes[i].meshVertices[0].vertex.z);
+    printf("Nodes count: %i\n", (int)meshdata.meshNodes.size());
+    for (int i = 0; i < meshdata.meshNodes.size(); i++){
+        printf("Vertex count: %i\n", (int)meshdata.meshNodes[i].meshVertices.size());
+        printf("Vertex: x: %f, y: %f, z: %f\n", meshdata.meshNodes[i].meshVertices[0].vertex.x, meshdata.meshNodes[i].meshVertices[0].vertex.y, meshdata.meshNodes[i].meshVertices[0].vertex.z);
+        printf("Index count: %i\n", (int)meshdata.meshNodes[i].indices.size());
+        printf("Texture: %s\n", meshdata.meshNodes[i].materials[0].texture.c_str());
     }
+
+    printf("--------------------------\n");
 
     printf("Signature: %s\n", readsig);
     printf("Version: %i\n", readversion);
-    for (int i = 0; i < meshNodesTest.size(); i++){
-        
-        printf("Vertex2 count: %i\n", (int)meshNodesTest[i].meshVertices.size());
-        printf("Vertex2: x: %f, y: %f, z: %f\n", meshNodesTest[i].meshVertices[0].vertex.x, meshNodesTest[i].meshVertices[0].vertex.y, meshNodesTest[i].meshVertices[0].vertex.z);
+
+    printf("Nodes count: %i\n", (int)meshDataTest.meshNodes.size());
+    for (int i = 0; i < meshDataTest.meshNodes.size(); i++){
+        printf("Vertex count: %i\n", (int)meshDataTest.meshNodes[i].meshVertices.size());
+        printf("Vertex: x: %f, y: %f, z: %f\n", meshDataTest.meshNodes[i].meshVertices[0].vertex.x, meshDataTest.meshNodes[i].meshVertices[0].vertex.y, meshDataTest.meshNodes[i].meshVertices[0].vertex.z);
+        printf("Index count: %i\n", (int)meshDataTest.meshNodes[i].indices.size());
+        printf("Texture: %s\n", meshDataTest.meshNodes[i].materials[0].texture.c_str());
     }
 
   return 0;
@@ -108,12 +133,36 @@ void writeMeshVerticesVector(std::ostream& os, const std::vector<MeshVertex> &ve
     os.write((char*)&vec[0], vec.size() * sizeof(MeshVertex));
 }
 
+void writeIndicesVector(std::ostream& os, const std::vector<unsigned int> &vec){
+    typename std::vector<unsigned int>::size_type size = vec.size();
+    os.write((char*)&size, sizeof(size));
+    os.write((char*)&vec[0], vec.size() * sizeof(unsigned int));
+}
+
+void writeString(std::ostream& os, const std::string &str){
+    typename std::string::size_type size = str.size();
+    os.write((char*)&size, sizeof(size));
+    os.write((char*)&str[0], size);
+}
+
+void writeMeshMaterialsVector(std::ostream& os, const std::vector<MeshMaterial> &vec){
+    typename std::vector<MeshMaterial>::size_type size = vec.size();
+    os.write((char*)&size, sizeof(size));
+
+    for (typename std::vector<MeshMaterial>::size_type i = 0; i < size; ++i){
+        os.write((char*)&vec[i].type, sizeof(int));
+        writeString(os, vec[i].texture);
+    }
+}
+
 void writeMeshNodesVector(std::ostream& os, const std::vector<MeshNode> &vec){
     typename std::vector<MeshNode>::size_type size = vec.size();
     os.write((char*)&size, sizeof(size));
 
     for (typename std::vector<MeshNode>::size_type i = 0; i < size; ++i){
         writeMeshVerticesVector(os, vec[i].meshVertices);
+        writeIndicesVector(os, vec[i].indices);
+        writeMeshMaterialsVector(os, vec[i].materials);
     }
 }
 
@@ -124,6 +173,31 @@ void readMeshVerticesVector(std::istream& is, std::vector<MeshVertex> &vec){
     is.read((char*)&vec[0], vec.size() * sizeof(MeshVertex));
 }
 
+void readIndicesVector(std::istream& is, std::vector<unsigned int> &vec){
+    typename std::vector<unsigned int>::size_type size = 0;
+    is.read((char*)&size, sizeof(size));
+    vec.resize(size);
+    is.read((char*)&vec[0], vec.size() * sizeof(unsigned int));
+}
+
+void readString(std::istream& is, std::string &str){
+    typename std::string::size_type size = 0;
+    is.read((char*)&size, sizeof(size));
+    str.resize(size);
+    is.read((char*)&str[0], size);
+}
+
+void readMeshMaterialsVector(std::istream& is, std::vector<MeshMaterial> &vec){
+    typename std::vector<MeshMaterial>::size_type size = 0;
+    is.read((char*)&size, sizeof(size));
+    vec.resize(size);
+
+    for (typename std::vector<MeshNode>::size_type i = 0; i < size; ++i){
+        is.read((char*)&vec[i].type, sizeof(int));
+        readString(is, vec[i].texture);
+    }
+}
+
 void readMeshNodesVector(std::istream& is, std::vector<MeshNode> &vec){
     typename std::vector<MeshVertex>::size_type size = 0;
     is.read((char*)&size, sizeof(size));
@@ -131,6 +205,8 @@ void readMeshNodesVector(std::istream& is, std::vector<MeshNode> &vec){
 
     for (typename std::vector<MeshNode>::size_type i = 0; i < size; ++i){
         readMeshVerticesVector(is, vec[i].meshVertices);
+        readIndicesVector(is, vec[i].indices);
+        readMeshMaterialsVector(is, vec[i].materials);
     }
 }
 
@@ -138,7 +214,7 @@ void processNode(aiNode *node, const aiScene *scene){
 
     for(unsigned int i = 0; i < node->mNumMeshes; i++){
         aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-        meshNodes.push_back(processMesh(mesh, scene));
+        meshdata.meshNodes.push_back(processMesh(mesh, scene));
     }
 
     for(unsigned int i = 0; i < node->mNumChildren; i++){
@@ -159,12 +235,12 @@ MeshNode processMesh(aiMesh *mesh, const aiScene *scene){
         meshVertex.vertex.y = mesh->mVertices[i].y;
         meshVertex.vertex.z = mesh->mVertices[i].z;
 
-        if(mesh->mTextureCoords[0]) {
-            meshVertex.texCoords.x = mesh->mTextureCoords[0][i].x; 
-            meshVertex.texCoords.y = mesh->mTextureCoords[0][i].y;
+        if(mesh->HasTextureCoords(0)) {
+            meshVertex.texcoord.x = mesh->mTextureCoords[0][i].x; 
+            meshVertex.texcoord.y = mesh->mTextureCoords[0][i].y;
         } else {
-            meshVertex.texCoords.x = 0.0f; 
-            meshVertex.texCoords.y = 0.0f;
+            meshVertex.texcoord.x = 0.0f; 
+            meshVertex.texcoord.y = 0.0f;
         }
 
         meshVertex.normal.x = mesh->mNormals[i].x;
@@ -182,6 +258,25 @@ MeshNode processMesh(aiMesh *mesh, const aiScene *scene){
         meshNode.meshVertices.push_back(meshVertex);
     }
 
+    for(unsigned int i = 0; i < mesh->mNumFaces; i++){
+        aiFace face = mesh->mFaces[i];
+        for(unsigned int j = 0; j < face.mNumIndices; j++)
+            meshNode.indices.push_back(face.mIndices[j]);
+    }
+
+    aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+    meshNode.materials = loadMaterialTextures(material, aiTextureType_DIFFUSE, 1);
+
     return meshNode;
 
+}
+
+std::vector<MeshMaterial> loadMaterialTextures(aiMaterial *mat, aiTextureType assimp_type, int type){
+    std::vector<MeshMaterial> material;
+    for(unsigned int i = 0; i < mat->GetTextureCount(assimp_type); i++){
+        aiString str;
+        mat->GetTexture(assimp_type, i, &str);
+        material.push_back({type, str.C_Str()});
+    }
+    return material;
 }
